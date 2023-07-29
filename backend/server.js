519c75cef9a6e20dc48a5b341852d2046623ca58
spykelionel <ndilionel6@gmail.com>
2023-07-29 07:34:48 +0100

    update server.js: setup listeners for socket.io connections

--- backend/server.js
+++ backend/server.js
@@ -1,69 +1,124 @@
-import path from 'path'
-import express, { json } from 'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import morgan from 'morgan'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js'
+import path from "path";
+import express, { json } from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import morgan from "morgan";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+import { Server } from "socket.io";
 
-import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
-import uploadRoutes from './routes/uploadRoutes.js'
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import Message from "./models/Message.js";
+import message from "./controllers/messageController.js";
+import messageRouter from "./routes/messageRoutes.js";
+import roomRouter from './routes/roomRoutes.js'
+import Room from "./models/Room.js";
 
-dotenv.config()
+dotenv.config();
 
 // Invoke connectDB
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
+// const server = createServer(app)
+// const socketIO = new Server(server);
 
-// Run morgan ONLY if in development mode
-// morgan logs all activities
-if (process.env.NODE_ENV === 'development') {
-	app.use(morgan('dev'))
+const io = new Server(process.env.SOCKET_PORT, {
+	cors: {
+		origin: "http://localhost:3000",
+	},
+});
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
-app.use(express.json())
+app.use(express.json());
+
+io.on("connection", (socket) => {
+  console.log("Connection established");
+
+  getMostRecentMessages()
+    .then((results) => {
+      socket.emit("mostRecentMessages", [...results.reverse()]);
+    })
+    .catch((error) => {
+      socket.emit("mostRecentMessages", []);
+    });
+
+  socket.on("newChatMessage", (data) => {
+    //send event to every single connected socket
+    console.log(data);
+    try {
+      const message = new Message(data);
+      const roomId = data.room;
+      message
+        .save()
+        .then((res) => {
+          console.log(res);
+          io.emit("newChatMessage", res);
+        })
+        .catch((error) => console.log("error:", error));
+    } catch (e) {
+      console.log("error:", e);
+    }
+  });
+  socket.on("disconnect", () => {
+    console.log("connection disconnected");
+  });
+});
+
+/**
+ * get 10 last messages
+ * @returns {Promise<Model[]>}
+ */
+async function getMostRecentMessages() {
+  return await message.getAllMessages();
+}
 
 // Mount routes to respective imports
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/orders', orderRoutes)
-app.use('/api/upload', uploadRoutes)
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/room", roomRouter);
+app.use("/api/message", messageRouter);
 
-app.get('/api/config/paypal', (req, res) =>
-	res.send(process.env.PAYPAL_CLIENT_ID)
-)
+app.get("/api/config/paypal", (req, res) =>
+  res.status(503).json({ message: "Unimplemented" })
+);
 
 // Make uploads folder static
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // Load build folder as static ONLY in production
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '/frontend/build')))
-	app.get('*', (req, res) =>
-		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-	)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 } else {
-	// test get route
-	app.get('/', (req, res) => {
-		res.send('API is running...')
-	})
+  // test get route
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
 }
 
 // Error middleware for 404
-app.use(notFound)
+app.use(notFound);
 
 // Error handler middleware
-app.use(errorHandler)
+app.use(errorHandler);
 
 // Set port number
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(
-	PORT,
-	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold
-	)
-)
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold
+  )
+);
